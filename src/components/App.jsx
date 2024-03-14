9cfcb945ad784adadd842f4cf4f39e1aa566a7be
serg444s <143083342+serg444s@users.noreply.github.com>
2024-03-14 21:49:34 +0200

    react

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import { Product } from "./Product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
