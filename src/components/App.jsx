fca8c13043da558c9d18fe5d33fcf0a7c46b4eff
serg444s <143083342+serg444s@users.noreply.github.com>
2024-03-19 19:43:42 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-// import { useState } from "react";
+import { useState } from 'react';
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import './App.css';
 import Product from './Product';
 
 export default function App() {
+  // let clicks = 0;
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    // clicks = clicks + 1;
+    setClicks(clicks + 1);
+  };
   return (
     <div>
+      <button onClick={handleClick}>Current: {clicks}</button>
       <h1>Best selling</h1>
       <Product
         name="Tacos With Lime"
